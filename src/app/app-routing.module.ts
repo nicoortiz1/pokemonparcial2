0184f4a08568dde0599100515f8033961d47218a
nicoortiz1 <nicoortizpe@gmail.com>
2023-06-23 19:03:55 -0300

    1.1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path:"**",
+    redirectTo:'tabs/tab1'
   }
 ];
 @NgModule({
