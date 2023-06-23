81ed0be6eab1cf76340784f3ca8687150d9e941d
JuanCruz0715 <juanchi97cruz@gmail.com>
2023-06-23 09:58:20 -0300

    1.0.7

--- src/app/services/pokemet.service.ts
+++ src/app/services/pokemet.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Result, RootObject } from '../interfaces/interfaces';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,6 +14,14 @@ export class PokeServicioService {
     return this.http.get<RootObject>("https://pokeapi.co/api/v2/type/");
     }
     
+    infoName: string="";
+    asignarName(name: string){
+      this.infoName = name;
+    }
 
-
-}
+    getNamePoke(name: string) {
+      return this.http.get<RootObject>("https://pokeapi.co/api/v2/pokemon/+name");
+    }
+    
+      
+    }
